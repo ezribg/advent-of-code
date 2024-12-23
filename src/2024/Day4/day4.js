157a2734bbb8e4d49dc81f7621f0c85eae23753c
Braid-Grizzell, Ezri <ebraidgrizzell@butler.edu>
2024-12-22 21:34:53 -0500

    working on 4

---
+++ src/2024/Day4/day4.js
@@ -0,0 +1,78 @@
+import { parseData, findTotalSum } from '../../utils.js'
+
+// const input = parseData('./input4.txt');
+const testInput = parseData('./input4-test.txt');
+
+
+const getCount = (data) => {
+    let count = 0;
+
+    for (let i = 0; i < data.length; i++) {
+        let splitString = data[i].split("XMAS");
+
+        count += splitString.length - 1;
+    }
+
+    return count
+}
+
+const reverseData = (data) => {
+    let newData = [];
+
+    for (let i = 0; i < data.length; i++) {
+        newData.push(data[i].split('').reverse().join(''));
+    }
+
+    return newData;
+}
+
+const getVerticalData = (data) => {
+    let newData = [];
+
+    for (let i = 0; i < data[0].length; i++) {
+        newData[i] = "";
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+            newData[j] += data[i][j];
+        }
+    }
+
+    return newData;
+}
+
+const getAllDirectionCounts = (data) => {
+    let count = 0;
+
+    // Horizontal
+    count += getCount(data);
+    count += getCount(reverseData(data));
+
+    // Vertical
+    let verticalData = getVerticalData(data);
+    count += getCount(verticalData);
+    count += getCount(reverseData(verticalData));
+
+    // Diagonal
+
+    return count;
+}
+
+const cleanData = (data) => {
+    for (let i = 0; i < data.length; i++) {
+        data[i] = data[i].replace('\r', '');
+    }
+
+    return data;
+}
+
+const solve1 = () => {
+    let data = cleanData(testInput);
+
+    console.log(getAllDirectionCounts(testInput));
+    // console.log(getVerticalData(data))
+    // console.log(data)
+};
+
+solve1();
--- src/day-template.js
+++ src/day-template.js
@@ -3,8 +3,13 @@ import { parseData, findTotalSum } from '../../utils.js'
 const input = parseData('./input0.txt');
 const testInput = parseData('./input0-test.txt');
 
-const solve = () => {
+const solve1 = () => {
 
 };
 
-solve();
+const solve2 = () => {
+
+};
+
+solve1();
+// solve2();
